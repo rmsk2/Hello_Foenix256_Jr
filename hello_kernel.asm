21fb3823bc386d9585432f65da2bca1d7be10ebe
Martin Grap <rmsk2@gmx.de>
2023-04-10 16:34:03 +0200

    Make Hello world using kernel routines work on real machine

--- hello_kernel.asm
+++ hello_kernel.asm
@@ -2,20 +2,22 @@
 ; target address is $4000
 * = $4000
 
-lda #0
-sta kernel.args.display.x
-lda #0
-sta kernel.args.display.y
-lda #<data
-sta kernel.args.display.text
-lda #>data
-sta kernel.args.display.text+1
-lda #14
-sta kernel.args.display.buflen
-lda #5
-sta kernel.args.display.color
-jsr kernel.Display.DrawRow
-rts
+     lda #0                                     ; set x coordinate
+     sta kernel.args.display.x
+     lda #0                                     ; set y coordinate
+     sta kernel.args.display.y
+     lda #<textData                                 ; set pointer to text data
+     sta kernel.args.display.text
+     lda #>textData
+     sta kernel.args.display.text+1
+     lda #12                                    ; set text length
+     sta kernel.args.display.buflen
+     lda #<colorData                            ; set pointer to color data (one byte for each byte of text)
+     sta kernel.args.display.color
+     lda #>colorData
+     sta kernel.args.display.color+1
+     jsr kernel.Display.DrawRow                 ; print to the screen
+     rts
 
-data .text "Hello World!"
-     .text $0d, $0a
+textData .text "Hello World!"
+colorData .text x"62" x 12
