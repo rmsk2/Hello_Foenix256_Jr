3fccadd83140a738a03ba73ee57c2e41dab3cbad
Martin Grap <rmsk2@gmx.de>
2023-04-22 12:07:26 +0200

    Document SLIP configuration script

--- slip_rpi.sh
+++ slip_rpi.sh
@@ -5,9 +5,14 @@
 # It also assumes that the Serial Line is operated at 115200 bps
 # and that the serial port used on the RasPi is /dev/ttyUSB0.
 
+# Creates sl0 interface
 slattach -s 115200 -p slip -L /dev/ttyUSB0 &
+#  Wait a bit so the changes made by slattach become visible
 sleep 2
+# Turns on ARP for interface sl0. This is probably unneccesary.
 ifconfig sl0 arp pointopoint
+# Configure local IP address and bring up interface
 ifconfig sl0 192.168.5.2
+# Route traffic for the F256 over the interface sl0
 route add 192.168.5.1 dev sl0
 echo SLIP started
