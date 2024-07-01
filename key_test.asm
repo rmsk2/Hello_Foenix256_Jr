8579366cb9cbcd0fdb5c93fbfc8d5223c8cc8ea9
Martin Grap <rmsk2@gmx.de>
2024-07-01 19:14:48 +0200

    Improve keytest program

--- key_test.asm
+++ key_test.asm
@@ -18,6 +18,8 @@ main
     jsr txtio.clear
     #locate 10, 5
     #printString startedTxt, len(startedTxt)
+    #locate 10, 7
+    #printString startedTxt2, len(startedTxt2)
     jsr txtio.newLine
     jsr txtio.newLine
 
@@ -29,19 +31,26 @@ _loop
     jsr kernel.NextEvent
     bcs _loop
     ; Handle the event
+    stz TYPE
     lda myEvent.type    
     cmp #kernel.event.key.PRESSED
     beq _done
     cmp #kernel.event.key.RELEASED
-    beq _done
+    beq _doneUp
     bra _loop
+_doneUp
+    inc TYPE
 _done
-    #printString MSG, len(MSG)
-    jsr txtio.newLine
     #printString MSG2, len(MSG2)
     jsr txtio.newLine
 
-
+    lda TYPE
+    beq _prDown
+    #printString T_UP, len(T_UP)
+    bra _goOn
+_prDown
+    #printString T_DWN, len(T_DWN)
+_goOn
     #printString FLAGS, len(FLAGS)
     lda myEvent.key.flags 
     jsr txtio.printByte
@@ -59,20 +68,24 @@ _done
 
     lda myEvent.key.ascii
     cmp #3
-    bne _loop
-
+    beq _stop 
+    jmp _loop
+_stop
     jsr txtio.cursorOn
     jsr restoreEvents
     rts
 
 
+TYPE .byte 0
 
 .include "khelp.asm"
 .include "txtio.asm"
 
-startedTxt .text "Press any key"
-MSG   .text "Contents of kernel event:"
+startedTxt .text "Press keys to see the kernel events which are generated", $0d
+startedTxt2 .text "Press Run/Stop or Ctrl+c to stop the program", $0d
 MSG2  .text "========================="
+T_UP  .text "Type : Up", $0d
+T_DWN .text "Type : Down", $0d
 FLAGS .text "flags: $"
 RAW   .text "raw  : $"
 ASCII .text "ascii: $"
